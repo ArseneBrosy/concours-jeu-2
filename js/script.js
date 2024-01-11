3a7c37c413d214a3bce01ea36d0fd1744d4ed372
ArseneBrosy <arsene.brosy@divtec.ch>
2024-01-11 20:11:49 +0100

    rotation

--- js/script.js
+++ js/script.js
@@ -1,27 +1,72 @@
+/**
+ * 2nd Game Jam with Alex
+ * @author Arsène Brosy
+ * @since 09.11.2024
+ */
+
 const canvas = document.querySelector("#game");
 const ctx = canvas.getContext("2d");
 
-const ROTATION_SPEED = 10;
+const ROTATION_SPEED = 0.1;
+const ROTATION_MAX_SPEED = 10;
+const ROTATION_FRICTION = 0.1;
+const CAR_WIDTH = 50;
+const CAR_HEIGHT = 100;
+const CAR_SPRITE = new Image();
+CAR_SPRITE.src = "images/car.png";
 
 let car = {
-  rotation: 0
-}
+  x: 100,
+  y: 100,
+  rotation: 0,
+  rVelocity: 0,
+  direction: 0
+};
 
 setInterval(() => {
   canvas.width = canvas.clientWidth;
   canvas.height = canvas.clientHeight;
 
-  ctx.fillRect(100, 100, 100, 100);
+  //region Physics
+  //region Car
+  // rotation
+  car.rVelocity += ROTATION_SPEED * car.direction;
+  car.rVelocity = Math.max(Math.min(car.rVelocity, ROTATION_MAX_SPEED), -ROTATION_MAX_SPEED);
+  // rotation friction
+  if (car.direction === 0) {
+    if (Math.abs(car.rVelocity) <= ROTATION_FRICTION) {
+      car.rVelocity = 0;
+    } else {
+      car.rVelocity += ROTATION_FRICTION * (car.rVelocity > 0 ? -1 : 1);
+    }
+  }
+  car.rotation += car.rVelocity;
+  //endregion
+  //endregion
+
+  //region Draw
+  //region Car
+  ctx.translate(car.x, car.y);
+  ctx.rotate(car.rotation * (Math.PI/180));
+  ctx.drawImage(CAR_SPRITE, -CAR_WIDTH / 2, -CAR_HEIGHT / 2, CAR_WIDTH, CAR_HEIGHT);
+  ctx.rotate(-car.rotation * (Math.PI/180));
+  ctx.translate(-car.x, -car.y);
+  //endregion
+  //endregion
 }, 0);
 
 document.addEventListener("keydown", (e) => {
-  console.log(e.code);
-
   if (e.code === "KeyA") {
-    car.rotation -= ROTATION_SPEED;
+    car.direction = -1;
   }
 
   if (e.code === "KeyD") {
-    car.rotation += ROTATION_SPEED;
+    car.direction = 1;
+  }
+});
+
+document.addEventListener("keyup", (e) => {
+  if (e.code === "KeyA" || e.code === "KeyD") {
+    car.direction = 0;
   }
 });
