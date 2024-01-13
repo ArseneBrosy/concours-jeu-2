e6af6ec584ab7f1b52ea43c9bc5a079bbdc62b3b
ArseneBrosy <arsene.brosy@divtec.ch>
2024-01-13 15:58:38 +0100

    better driving + turning no drifts anymore (need to add)

--- js/script.js
+++ js/script.js
@@ -7,17 +7,15 @@
 const canvas = document.querySelector("#game");
 const ctx = canvas.getContext("2d");
 
-const DEBUG_MODE = true;
+const DEBUG_MODE = false;
 
 const ROTATION_SPEED = 0.02;
 const ROTATION_MAX_SPEED = 2;
 const ROTATION_FRICTION = 0.03;
-const ACCELERATION_SPEED = 0.01;
 const MAX_STEER_ANGLE = 30;
 const WHEEl_TURN_SPEED = 1;
-const SPEED = 2;
 const CAR_WIDTH = 50;
-const CAR_HEIGHT = 100;
+const CAR_LENGTH = 100;
 const CAR_SPRITE = new Image();
 CAR_SPRITE.src = "images/car.png";
 
@@ -30,17 +28,41 @@ let car = {
     y: 0,
     magnitude: 0
   },
-  xTargetVelocity: 0,
-  yTargetVelocity: 0,
   frontForce: {
     x: 0,
     y: 0,
   },
+  speed: 0,
   steer_angle: 0,
-  rVelocity: 0,
   direction: 0,
-  running: false
+  running: false,
+
+  // constants
+  maxSpeed: 3,
+  acceleration: 0.01
 };
+
+function round(x, decimals = 3) {
+  return Math.floor(x * 10**decimals) / 10**decimals;
+}
+
+function moveCar(steer, speed, length) {
+  const directionAngle = 180 - steer;
+  const prop = Math.sin(directionAngle * (Math.PI/180)) / length;
+  const turnAngle = round(Math.asin(speed * prop) * (180/Math.PI));
+  const x = Math.cos(steer * (Math.PI/180)) * speed + length / 2;
+  const y = Math.cos(turnAngle * (Math.PI/180)) * length;
+  const xOffset = -0.5 * y + x;
+  const yOffset = Math.sin(turnAngle * (Math.PI/180)) * length / 2;
+  return [turnAngle, xOffset, yOffset];
+}
+
+function localToWorldTransform(rotation, x, y) {
+  rotation -= 90;
+  const localX = round(x * Math.cos(rotation * (Math.PI / 180)) - y * Math.sin(rotation * (Math.PI / 180)));
+  const localY = round(x * Math.sin(rotation * (Math.PI / 180)) + y * Math.cos(rotation * (Math.PI / 180)));
+  return [localX, localY];
+}
 
 setInterval(() => {
   canvas.width = canvas.clientWidth;
@@ -48,51 +70,29 @@ setInterval(() => {
 
   //region Physics
   //region Car
-  // rotation
+  // speed
+  if (Math.abs(car.speed - car.maxSpeed * car.running) <= car.acceleration) {
+    car.speed = car.maxSpeed * car.running;
+  } else {
+    car.speed += car.acceleration * (car.speed < car.maxSpeed * car.running ? 1 : -1);
+  }
+
+  // wheel direction
   const frontForceTarget = MAX_STEER_ANGLE * car.direction;
   if (Math.abs(car.steer_angle - frontForceTarget) <= WHEEl_TURN_SPEED) {
     car.steer_angle = frontForceTarget;
   } else {
     car.steer_angle += WHEEl_TURN_SPEED * (car.steer_angle < frontForceTarget ? 1 : -1);
   }
-  car.frontForce.x = Math.sin((car.rotation + car.steer_angle) * (Math.PI/180)) * SPEED * car.running;
-  car.frontForce.y = Math.cos((car.rotation + car.steer_angle) * (Math.PI/180)) * -SPEED * car.running;
-  /*
-  car.rVelocity += ROTATION_SPEED * car.direction;
-  car.rVelocity = Math.max(Math.min(car.rVelocity, ROTATION_MAX_SPEED), -ROTATION_MAX_SPEED);
-  // rotation friction
-  if (car.direction === 0) {
-    if (Math.abs(car.rVelocity) <= ROTATION_FRICTION) {
-      car.rVelocity = 0;
-    } else {
-      car.rVelocity += ROTATION_FRICTION * (car.rVelocity > 0 ? -1 : 1);
-    }
-  }
-  car.rotation += car.rVelocity;
-  */
+  car.frontForce.x = Math.sin((car.rotation + car.steer_angle) * (Math.PI/180)) * car.speed;
+  car.frontForce.y = Math.cos((car.rotation + car.steer_angle) * (Math.PI/180)) * -car.speed;
 
-  // speed
-  /*
-  // target velocity
-  car.xTargetVelocity = Math.sin(car.rotation * (Math.PI/180)) * SPEED * car.running;
-  car.yTargetVelocity = Math.cos(car.rotation * (Math.PI/180)) * SPEED * car.running * -1;
-  // actual velocity
-  if (Math.abs(car.xTargetVelocity - car.velocity.x) <= ACCELERATION_SPEED) {
-    car.velocity.x = car.xTargetVelocity;
-  } else {
-    car.velocity.x += ACCELERATION_SPEED * (car.xTargetVelocity > car.velocity.x ? 1 : -1);
-  }
-  if (Math.abs(car.yTargetVelocity - car.velocity.y) <= ACCELERATION_SPEED) {
-    car.velocity.y = car.yTargetVelocity;
-  } else {
-    car.velocity.y += ACCELERATION_SPEED * (car.yTargetVelocity > car.velocity.y ? 1 : -1);
-  }
-  // calculate velocity
-  car.velocity.magnitude = Math.sqrt(car.velocity.x**2 + car.velocity.y**2);
-  // move car
-  car.x += car.velocity.x;
-  car.y += car.velocity.y;
-  */
+  // turning
+  const transformValues = moveCar(car.steer_angle, car.speed, CAR_LENGTH);
+  const localTranslate = localToWorldTransform(car.rotation, transformValues[1], transformValues[2]);
+  car.rotation += transformValues[0];
+  car.x += localTranslate[0];
+  car.y += localTranslate[1];
   //endregion
   //endregion
 
@@ -100,7 +100,7 @@ setInterval(() => {
   //region Car
   ctx.translate(car.x, car.y);
   ctx.rotate(car.rotation * (Math.PI/180));
-  ctx.drawImage(CAR_SPRITE, -CAR_WIDTH / 2, -CAR_HEIGHT / 2, CAR_WIDTH, CAR_HEIGHT);
+  ctx.drawImage(CAR_SPRITE, -CAR_WIDTH / 2, -CAR_LENGTH / 2, CAR_WIDTH, CAR_LENGTH);
   ctx.rotate(-car.rotation * (Math.PI/180));
   ctx.translate(-car.x, -car.y);
   //endregion
@@ -109,26 +109,18 @@ setInterval(() => {
   if (DEBUG_MODE) {
     // Velocity
     ctx.lineWidth = 2;
-    const mul = 100 / SPEED;
-    // target
-    ctx.strokeStyle = "blue";
-    ctx.beginPath();
-    ctx.moveTo(car.x, car.y);
-    ctx.lineTo(car.x + car.xTargetVelocity * mul, car.y + car.yTargetVelocity * mul);
-    ctx.stroke();
-    // actual
-    ctx.strokeStyle = "red";
-    ctx.beginPath();
-    ctx.moveTo(car.x, car.y);
-    ctx.lineTo(car.x + car.velocity.x * mul, car.y + car.velocity.y * mul);
-    ctx.stroke();
+    const mul = 100 / car.maxSpeed;
     // front
     ctx.strokeStyle = "green";
     ctx.beginPath();
-    const fmul = 100 / Math.sqrt(car.frontForce.x**2 + car.frontForce.y**2);
-    ctx.moveTo(car.x + Math.sin(car.rotation * (Math.PI/180)) * CAR_HEIGHT / 2, car.y + Math.cos(car.rotation * (Math.PI/180)) * -CAR_HEIGHT / 2);
-    ctx.lineTo(car.x + Math.sin(car.rotation * (Math.PI/180)) * CAR_HEIGHT / 2 + car.frontForce.x * fmul, car.y + Math.cos(car.rotation * (Math.PI/180)) * -CAR_HEIGHT / 2 + car.frontForce.y * fmul);
+    ctx.moveTo(car.x + Math.sin(car.rotation * (Math.PI/180)) * CAR_LENGTH / 2, car.y + Math.cos(car.rotation * (Math.PI/180)) * -CAR_LENGTH / 2);
+    ctx.lineTo(car.x + Math.sin(car.rotation * (Math.PI/180)) * CAR_LENGTH / 2 + car.frontForce.x * mul, car.y + Math.cos(car.rotation * (Math.PI/180)) * -CAR_LENGTH / 2 + car.frontForce.y * mul);
     ctx.stroke();
+    // transform
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(car.x + localTranslate[0] * mul, car.y + localTranslate[1] * mul, 5, 0, 2 * Math.PI);
+    ctx.fill();
     //endregion
   }
   //endregion
